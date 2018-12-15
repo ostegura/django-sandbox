46873884098688d023602d643139104be8472ff5
ostegura <stegura99@gmail.com>
2018-12-16 00:11:34 +0200

    Basic level is done

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'GMT+2'
+TIME_ZONE = 'CET'
 
 USE_I18N = True
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -47,5 +47,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results',
-                                            args=(question_id)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
